0948a54fc86bbb438c7ce7770043775cb0e50706
jaeyun <sin4691@naver.com>
2025-04-03 16:45:35 +0900

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind + TypeScript 정상 작동! 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
