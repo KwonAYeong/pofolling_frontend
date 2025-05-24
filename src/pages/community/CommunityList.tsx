810587c15b06365fad4dd24f81ebc19d713a1fd1
Kimhyomin <khm031019@gmail.com>
2025-05-24 23:58:55 +0900

    chore: 커뮤니티 수정페이지1

--- src/pages/community/CommunityList.tsx
+++ src/pages/community/CommunityList.tsx
@@ -25,8 +25,9 @@ const CommunityList = () => {
         const res = await axios.get('http://localhost:8080/community/post');
         const content = res.data.data.content;
 
+        // 최신순 정렬 (createdAt 기준 내림차순)
         const sorted = content.sort(
-          (a: Post, b: Post) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+          (a: Post, b: Post) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
         );
 
         setPosts(sorted);
