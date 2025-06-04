0a0a6dc40105e4ed8532f1713aafb4c20f062072
KwonAYeong <129161150+KwonAYeong@users.noreply.github.com>
2025-06-04 10:18:17 +0900

    feat: 카카오 소셜 로그인 기능 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080", // 백엔드 주소
-  withCredentials: true,
+  baseURL: 'http://localhost:8080', // 백엔드 API 주소
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// JWT 자동 추가
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default instance;
